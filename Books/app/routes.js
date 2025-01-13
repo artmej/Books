fad221090552b71fa13fc54e405eb4ae1c35cb73
artmej <60410237+artmej@users.noreply.github.com>
2025-01-13 13:36:33 -0600

    Update routes.js
    adecuación para ejecutar delete.

--- Books/app/routes.js
+++ Books/app/routes.js
@@ -1,38 +1,54 @@
 var path = require('path');
 var Book = require('./model');
 var routes = function(app) {
-    app.get('/book', function(req, res) {
-        Book.find({}, function(err, result) {
-            if ( err ) throw err;
-            res.json(result);
-        });
+    app.get('/book', async function(req, res) {
+        try {
+            const results = await Book.find({});
+            res.json(results);
+        } catch (err) {
+            res.send(err);
+        }
     });
-    app.post('/book', function(req, res) {
-        var book = new Book( {
-            name:req.body.name,
-            isbn:req.body.isbn,
-            author:req.body.author,
-            pages:req.body.pages
+
+    app.post('/book', async function(req, res) {
+        var book = new Book({
+            name: req.body.name,
+            isbn: req.body.isbn,
+            author: req.body.author,
+            pages: req.body.pages
         });
-        book.save(function(err, result) {
-            if ( err ) throw err;
-            res.json( {
-                message:"Successfully added book",
-                book:result
-            });
-        });
-    });
-    app.delete("/book/:isbn", function(req, res) {
-        Book.findOneAndRemove(req.query, function(err, result) {
-            if ( err ) throw err;
-            res.json( {
-                message: "Successfully deleted the book",
+
+        try {
+            const result = await book.save();
+            res.json({
+                message: "Successfully added book",
                 book: result
             });
-        });
+        } catch (err) {
+            res.send(err);
+        }
     });
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname + '/public', 'index.html'));
-    });
-};
-module.exports = routes;
+
+    app.delete('/book/:isbn', async function(req, res) {
+        try {
+            const isbn = req.params.isbn;
+            console.log('ISBN to delete:', isbn);
+            const result = await Book.deleteOne({ isbn: isbn.toString() });
+            if (result) {
+                res.json({
+                        message: "Successfully deleted the book",
+                        book: result
+                });
+            } else {
+                        console.log('Book not found'); // Log en caso de no encontrar el libro
+                        res.status(404).json({ message: "Book not found" });
+                    }
+                } catch (err) {
+                    res.send(err);
+                }
+            });
+        
+            app.get('*', function(req, res) {
+                res.sendFile(path.join(__dirname + '/public', 'index.html'));
+            });
+        };
