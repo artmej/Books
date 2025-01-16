90c8633d7d1d2b9b015df1702d69b0d3be2afe73
artmej <60410237+artmej@users.noreply.github.com>
2025-01-15 19:36:01 -0600

    requiered a los objetos

--- Books/app/model.js
+++ Books/app/model.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 var dbHost = 'mongodb://localhost:27017/Books';
-mongoose.connect(dbHost, { useUnifiedTopology: true } );
+mongoose.connect(dbHost, { useNewUrlParser: true,  useUnifiedTopology: true } );
 mongoose.connection;
 mongoose.set('debug', true);
 var bookSchema = mongoose.Schema( {
-    name: String,
-    isbn: {type: String, index: true},
-    author: String,
-    pages: Number
+  name: { type: String, required: true },
+  isbn: { type: String, required: true, unique: true, dropDups: true},
+  author: { type: String, required: true },
+  pages: { type: Number, required: true }    
 });
 var Book = mongoose.model('Book', bookSchema);
 module.exports = Book;
